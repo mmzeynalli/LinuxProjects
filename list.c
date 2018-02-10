43c05790855a04db9f69e7d47b78205436ea3642
mmzeynalli <mmzeynall1998@gmail.com>
2018-02-10 19:59:14 +0400

    INFO 1. Updates Done: a. Codes are collected into functions b. Sorting function is added c. ANSI codes of colors are defined d. Number of files is determined before initializing buffer e. Buffer is freed and directory is closed at the end
    2. Errors/Problems to handle:
    a. No particular problems
    
    3. Updates to Do:
    a. Determine the type of files (directories, executables etc.)
    b. Not show .swp files in ls command (without an argument)
    c. Ignore symbols in file name while sorting

--- list.c
+++ list.c
@@ -2,12 +2,28 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
+#include <malloc.h>
+#include <ctype.h>
 
 /* DEFINES */
+
+#define DIR_CUR "."
+#define DIR_PREV ".."
+
+#define COLOR_GREEN "\x1b[1;32m" //color for executables
+#define COLOR_TURQUOISE "\x1b[1;96m" //color for directories
+#define COLOR_BACKG_RED "\x1b[0;41m"
+#define COLOR_RESET "\x1b[0m"
 
 /* END OF DEFINES */
 
 /* FUNCTION PROTOTYPES */
+
+int getNumberOfFilesInDIR(); //number of files in current directory
+void storeAndSortFileNames(char*** buff); //store filenames in buffer and sort them
+int cmp(const void* s1, const void* s2); //comporator for sorting
+char* strlwr(const char* s); //makes string to be lower cased
+void ls(); //lists all in the current directory
 
 /* END OF FUNCTION PROTOTYPES */
 
@@ -18,6 +34,7 @@ DIR* dir;
 struct dirent* file;
 int i;
 int cnt = 0;
+char** buff = NULL; // for storing filenames
 
 /* END OF GLOBAL VARIABLES */
 
@@ -29,32 +46,51 @@ int main(int argc, char** argv)
     exit(1);
   }
 //----------------------------------------
-  if (argc == 1)
+  if (argc == 1) //correct swap file and sorting (- is not considered)
   {
-    dir = opendir(".");
-    char **buff;
-
-    buff = calloc(100, sizeof(char *));
-    
-    while((file = readdir(dir)) != NULL)
-    {
-      sprintf(buff[cnt++], "%s", file->d_name);
-      //buff[cnt++] = file->d_name;
-    }
+    ls();
     
     for(i = 0; i < cnt; i++)
-      printf("%s  ", buff[i]);
-
+      if(strcmp(buff[i], DIR_CUR) && strcmp(buff[i], DIR_PREV))
+      	printf("%s%s%s  ", COLOR_TURQUOISE, buff[i], COLOR_RESET);
+    
     printf("\n");
+
+    free(buff);
+    closedir(dir);
 
     return 0;
   }
-
-  for (i = 0; i < argc; i++)
+//---------------------------------------
+  if(!strcmp(argv[1], "-A"))
   {
-    printf("%s", argv[i]);
+    ls();
+    
+    for(i = 0; i < cnt; i++)
+      if(strcmp(buff[i], DIR_CUR) && strcmp(buff[i], DIR_PREV))
+      	printf("%s  ", buff[i]);
+    
+    printf("\n");
 
-    //check for arguments
+    free(buff);
+    closedir(dir);
+
+    return 0;
+  }
+//--------------------------------------
+  if(!strcmp(argv[1], "-a"))
+  {
+    ls();
+    
+    for(i = 0; i < cnt; i++)
+      	printf("%s  ", buff[i]);
+    
+    printf("\n");
+
+    free(buff);
+    closedir(dir);
+
+    return 0;
   }
 
 //-----------------------------------------  
@@ -64,6 +100,66 @@ int main(int argc, char** argv)
 }
 
 /* FUNCTIONS */
+
+void ls()
+{
+
+  cnt = getNumberOfFilesInDIR(); 
+  buff = calloc(cnt, sizeof(char*));
+    
+  cnt = 0; //reset value for next iterations
+
+  storeAndSortFileNames(&buff);
+
+}
+
+int getNumberOfFilesInDIR()
+{
+  int n = 0;
+  dir = opendir(DIR_CUR);
+  
+  while((file = readdir(dir)) != NULL)
+  { 
+    n++;
+  }
+
+  rewinddir(dir);
+
+  return n; //all files + 2 directories (. & ..)
+}
+
+void storeAndSortFileNames(char*** buff)
+{
+
+  while((file = readdir(dir)) != NULL)
+  { 
+    (*buff)[cnt++] = file->d_name;
+  }
+
+  qsort(*buff, cnt, sizeof(char *), cmp);
+}
+
+int cmp(const void* s1, const void* s2)
+{
+  const char* a = *(const char**) s1;
+  const char* b = *(const char**) s2;
+
+  return strcmp(strlwr(a), strlwr(b));
+}
+
+char* strlwr(const char* s)
+{
+  char* temp;
+
+  temp = calloc(strlen(s), sizeof(char));
+  
+  for(i = 0; i < strlen(s); i++)
+  {
+    temp[i] = tolower(s[i]);
+  }
+  
+  return temp;
+}
 
 /* END OF FUNCTIONS */
 

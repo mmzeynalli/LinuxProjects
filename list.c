4d94798d30fdce1f2dc97defe349e902619f1f1c
mmzeynalli <mmzeynall1998@gmail.com>
2018-02-18 19:05:20 +0400

    INFO 1. Updates Done: a. For getting arguments from user, struct is created b. Filenames are not stored in buffer anymore, there is new struct where all details about the file are stored c. Two more comparator functions (for sorting by time and size) d. More functions were added, such as userID, groupID, groupName etc.
    2. Errors/Problems to handle:
    a. Get valid data from time_t variables such as atime, ctime and mtime
    b. Make sure all overriding arguments are handled
    c. Format the output
    
    3. Uptades to Do:
    a. Reduce number of get functions that returns data about the file
    b. Add code for recursive listing
    c. Add some more details to getPermission function (l, s, S values)
    d. Add code for three more argumets: -w, -q and -s

--- list.c
+++ list.c
@@ -7,6 +7,9 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <errno.h>
+#include <time.h>
+#include <pwd.h>
+#include <grp.h>
 
 /* DEFINES */
 
@@ -21,178 +24,256 @@
 
 /* END OF DEFINES */
 
-/* FUNCTION PROTOTYPES */
+/* STRUCTURES */
+typedef struct {
 
-int getNumberOfFilesInDIR(); // number of files in current directory
-void storeAndSortFileNames(char*** buff); // store filenames in buffer and sort them
-int cmp(const void* s1, const void* s2); // comporator for sorting
-char* strlwr(const char* s); // makes string to be lower cased
-void ls(); // lists all in the current directory
-char* permColor(char* file); // depending on permissions, output color of file name changes
-int isHiddenFile(char* file); // checks if file is hidden or not
-void printExtension(char* COLOR); // depending on permission, prints extension
-int inode(char* file); // finds i-node of file
+  char* name;
+  char* permission;
+  char* color;
+  char exe;
 
-/* END OF FUNCTION PROTOTYPES */
+  char* userName;
+  char* groupName;
 
+  time_t mtime; // modification time
+  time_t ctime; // file status change time
+  time_t atime; // access time
+
+  off_t size;
+  int inode;
+  int nlink;
+  int blocks;
+
+  int userID;
+  int groupID;
+
+
+} fileInfo;
+
+typedef struct 
+{
+  int show_hidden;
+  int show_inode;
+  int show_idNumbers;
+  int show_Recursive;
+  int show_CUR_PREV_DIR;
+  int show_extensions;
+  int show_longList;
+  int show_readableB;
+  int show_readableK;
+  int show_color;
+  int show_contentOfDirs;
+  int show_multiCol;
+  int show_perLine;
+
+  int no_sort;
+  int sort_reverse;
+  int sort_size;
+  int sort_mtime;
+  int sort_ctime; // status change time
+  int sort_atime; // access time		
+
+} Options;
+
+/* END OF STRUCTURES */
 
 /* GLOBAL VARIABLES */
 
 DIR* dir;
 struct dirent* file;
-struct stat st;
+struct stat st; 
+struct tm Date;
+struct group* grp;
+struct passwd* pwd;
+
 int i;
 int cnt = 0;
-char** buff = NULL; // for storing filenames
-int toSort = 1; // 0 - not sort, 1 - sort
+Options op;
+fileInfo *entry;
 
 /* END OF GLOBAL VARIABLES */
+
+/* FUNCTION PROTOTYPES */
+
+void ls(); // lists all in the current directory
+void initOptions(char* str); // initialize arguments
+void initStruct();
+void storeFileInfo(); // store filenames in buffer and sort them
+
+int getNumOfFilesInDIR(); // number of files in current directory
+int getNumOfHLinks();
+int get_iNode(char* file); // finds i-node of file
+int get_aTime(char* file); // access time
+int get_cTime(char* file); // status change time
+int get_mTime(char* file); // modification time
+int getUserID(char* file); // id of user
+int getGroupID(char* file); // id of group
+char getExtension(char* COLOR); // depending on permission, prints extension
+char* getPermission(char* file); // returns permissions of file
+char* getColor(char* file); // depending on permissions, output color of file name changes
+char* getUserName(char* file);
+char* getGroupName(char* file);
+
+int cmpName(const void* s1, const void* s2); // comparator for sorting by alphebetical order
+int cmpTime(const void* s1, const void* s2); // comparator for sorting by time
+int cmpSize(const void* s1, const void* s2); // comparator for sorting by file size
+char* strlwr(const char* s); // makes string to be lower cased
+
+
+int isHiddenFile(char* file); // checks if file is hidden or not
+
+/* END OF FUNCTION PROTOTYPES */
 
 int main(int argc, char** argv)
 {
 
   if (argc < 1)
   {
-    perror("Not enough arguments!");
-    return strerror(errno);
+    perror("Not enough arguments!\n");
+    exit(1);
   }
+
 //----------------------------------------
-  if (argc == 1) //correct swap file and sorting (- is not considered)
+  if (argc == 1) //sorting (- is not considered)
   {
     ls();
-    
-    for(i = 0; i < cnt; i++)
-      if(strcmp(buff[i], DIR_CUR) && strcmp(buff[i], DIR_PREV) && !isHiddenFile(buff[i]))
-      	printf("%s%s%s  ", permColor(buff[i]), buff[i], COLOR_RESET);
-    
-    printf("\n");
 
-    free(buff);
+    // Date = gmtime(&entry[i].mtime);
+    // Date->tm_hour;
+    
+    for(int i = 0; i < cnt; i++)
+      printf("%s %d %s %s %lu %s%s%s\n", entry[i].permission, entry[i].nlink, entry[i].userName, entry[i].groupName, entry[i].size, entry[i].color, entry[i].name, COLOR_RESET);
+
     closedir(dir);
 
-    return 0;
+    exit(0);
   }
 //---------------------------------------
-  if(!strcmp(argv[1], "-A"))
+  else
   {
-    ls();
     
-    for(i = 0; i < cnt; i++)
-      if(strcmp(buff[i], DIR_CUR) && strcmp(buff[i], DIR_PREV))
-      	printf("%s%s%s  ", permColor(buff[i]), buff[i], COLOR_RESET);
-    
-    printf("\n");
-
-    free(buff);
-    closedir(dir);
-
-    return 0;
   }
-//--------------------------------------
-  if(!strcmp(argv[1], "-a"))
-  {
-    ls();
-    
-    for(i = 0; i < cnt; i++)
-      	printf("%s%s%s  ", permColor(buff[i]), buff[i], COLOR_RESET);
-    
-    printf("\n");
-
-    free(buff);
-    closedir(dir);
-
-    return 0;
-  }
-
-//-----------------------------------------  
-  if(!strcmp(argv[1], "-d")) //understand this argument
-  {
-    printf(COLOR_LIGHT_BLUE ".\n");
-  }
-//-----------------------------------------
-  if(!strcmp(argv[1], "-F"))
-  {
-    ls();
-    
-    for(i = 0; i < cnt; i++)
-      if(strcmp(buff[i], DIR_CUR) && strcmp(buff[i], DIR_PREV) && !isHiddenFile(buff[i]))
-      {
-	printf("%s%s%s", permColor(buff[i]), buff[i], COLOR_RESET);
-	printExtension(permColor(buff[i]));
-        
-      }
-
-    printf("\n");
-
-    free(buff);
-    closedir(dir);
-
-    return 0;
-  }
-
-  if(!strcmp(argv[1], "-f"))
-  {
-    toSort = 0;
-    ls();
-    
-    for(i = 0; i < cnt; i++)
-      	printf("%s  ",buff[i]);
-    
-    printf("\n");
-
-    free(buff);
-    closedir(dir);
-
-    return 0;
-  }
-//--------------------------------------------------
-
-  if (!strcmp(argv[1], "-i")) //correct swap file and sorting (- is not considered)
-  {
-    ls();
-    
-    for(i = 0; i < cnt; i++)
-      if(strcmp(buff[i], DIR_CUR) && strcmp(buff[i], DIR_PREV) && !isHiddenFile(buff[i]))
-      	printf("%d %s%s%s  ", inode(buff[i]), permColor(buff[i]), buff[i], COLOR_RESET);
-    
-    printf("\n");
-
-    free(buff);
-    closedir(dir);
-
-    return 0;
-  }
-
-  if(!strcmp(argv[1], "-1"))
-  {
-    ls();
-    
-    for(i = 0; i < cnt; i++)
-      if(strcmp(buff[i], DIR_CUR) && strcmp(buff[i], DIR_PREV) && !isHiddenFile(buff[i]))
-      	printf("%s%s%s\n", permColor(buff[i]), buff[i], COLOR_RESET);
-
-    free(buff);
-    closedir(dir);
-
-    return 0;
-  }
+  
 }
 
 /* FUNCTIONS */
 
+void initOptions(char* str)
+{
+  for(i = 1; i < strlen(str); i++)
+  {
+    switch(str[i]) // -w, -q, -s remains
+    {
+      case 'A': op.show_hidden = 1;
+		op.show_CUR_PREV_DIR = 0; 
+		break;
+      case 'a': op.show_hidden = 1; 
+		op.show_CUR_PREV_DIR = 1;
+		break;
+      case 'C': op.show_multiCol = 0;
+		op.show_perLine = 0;
+		break;
+      case 'c': op.sort_ctime = 1;
+		op.sort_size = 0;
+		op.sort_mtime = 0;
+		op.sort_atime = 0;
+		break;
+      case 'd': op.show_Recursive = 0; 
+		break;
+      case 'F': op.show_extensions = 1; 
+		break;
+      case 'f': op.no_sort = 1;
+		op.show_color = 0;
+		break;
+      case 'h': op.show_readableB = 1; 
+		break;
+      case 'i': op.show_inode = 1; 
+		break;
+      case 'k': op.show_readableK = 1; 
+		break;
+      case 'l': op.show_longList = 1; 
+		op.show_perLine = 1;
+		op.show_multiCol = 0;
+		break;
+      case 'n': op.show_longList = 1;
+		op.show_idNumbers = 1; 
+		op.show_perLine = 1;
+		op.show_multiCol = 0;
+		break;
+      case 'R': op.show_contentOfDirs = 1;
+		break;
+      case 'r': op.sort_reverse = 1;
+		break;
+      case 'S': op.sort_size = 1;
+		op.sort_atime = 0;
+		op.sort_ctime = 0;
+		op.sort_mtime = 0;
+		op.sort_reverse = 1;
+		break;
+      case 't': op.sort_mtime = 1;
+		op.sort_size = 0;
+		op.sort_atime = 0;
+		op.sort_ctime = 0;
+		break;
+      case 'u': op.sort_atime = 1;
+		op.sort_size = 0;
+		op.sort_mtime = 0;
+		op.sort_ctime = 0;
+		break;
+      case 'x': op.show_multiCol = 0;
+		op.show_perLine = 0;
+		break;
+      case '1': op.show_perLine = 1;
+		op.show_multiCol = 0;
+		break;
+
+    }
+  }
+}
+
 void ls()
 {
 
-  cnt = getNumberOfFilesInDIR(); 
-  buff = calloc(cnt, sizeof(char*));
-    
-  cnt = 0; //reset value for next iterations
+  cnt = getNumOfFilesInDIR();
+  
+  entry = calloc(cnt, sizeof(*entry));
 
-  storeAndSortFileNames(&buff);
+  cnt = 0; // reset value for next iterations
+
+  storeFileInfo();
 
 }
 
-int getNumberOfFilesInDIR()
+void storeFileInfo()
+{
+
+  while((file = readdir(dir)) != NULL)
+  { 
+    char* name = file->d_name;
+
+    entry[cnt].name = name;
+    entry[cnt].permission = getPermission(name);
+    entry[cnt].color = getColor(name);
+    entry[cnt].exe = getExtension(getColor(name));
+    entry[cnt].atime = get_aTime(name);
+    entry[cnt].ctime = get_cTime(name);
+    entry[cnt].mtime = get_mTime(name);
+    entry[cnt].nlink = getNumOfHLinks(name);
+    entry[cnt].userID = getUserID(name);
+    entry[cnt].groupID = getGroupID(name);
+    entry[cnt].userName = getUserName(name);
+    entry[cnt].groupName = getGroupName(name);
+    entry[cnt].inode = get_iNode(name);
+
+    cnt++;
+  }
+
+}
+
+
+/* GET FUNCTIONS */
+
+int getNumOfFilesInDIR()
 {
   int n = 0;
   dir = opendir(DIR_CUR);
@@ -207,24 +288,178 @@ int getNumberOfFilesInDIR()
   return n; //all files + 2 directories (. & ..)
 }
 
-void storeAndSortFileNames(char*** buff)
+int getNumOfHLinks(char* file)
 {
+  if(lstat(file, &st) == 0) return st.st_nlink;
 
-  while((file = readdir(dir)) != NULL)
-  { 
-    (*buff)[cnt++] = file->d_name;
+  perror("Could not get number of hard links of the file");
+  exit(1);
+}
+
+char* getPermission(char* file)
+{
+  char *modeVal = calloc(10, sizeof(char));
+    
+  if(lstat(file, &st) == 0)
+  {
+    mode_t perm = st.st_mode;
+    modeVal[0] = S_ISDIR(st.st_mode) ? 'd' : '-';
+    modeVal[1] = (perm & S_IRUSR) ? 'r' : '-';
+    modeVal[2] = (perm & S_IWUSR) ? 'w' : '-';
+    modeVal[3] = (perm & S_IXUSR) ? 'x' : '-';
+    modeVal[4] = (perm & S_IRGRP) ? 'r' : '-';
+    modeVal[5] = (perm & S_IWGRP) ? 'w' : '-';
+    modeVal[6] = (perm & S_IXGRP) ? 'x' : '-';
+    modeVal[7] = (perm & S_IROTH) ? 'r' : '-';
+    modeVal[8] = (perm & S_IWOTH) ? 'w' : '-';
+    modeVal[9] = (perm & S_IXOTH) ? 'x' : '-';
+    modeVal[10] = '\0';
+
+    return modeVal;     
   }
+  else
+  {
+    return strerror(errno);
+  }   
 
-  if(toSort) qsort(*buff, cnt, sizeof(char *), cmp);
 }
 
-int cmp(const void* s1, const void* s2)
+char* getColor(char *file)
 {
-  const char* a = *(const char**) s1;
-  const char* b = *(const char**) s2;
 
-  return strcmp(strlwr(a), strlwr(b));
+  if(lstat(file, &st) == 0)
+  {
+    mode_t perm = st.st_mode;
+
+    switch(perm & S_IFMT)
+    {
+      case S_IFLNK: return COLOR_CYAN;
+      case S_IFDIR: return COLOR_LIGHT_BLUE;
+      case S_IFSOCK: return COLOR_BACKG_RED;
+      case S_IFREG: return COLOR_RESET;
+
+    }
+  }
+  else
+  {
+    perror("Could not get permissions of file");
+    return strerror(errno);
+  }   
 }
+
+int getUserID(char* file)
+{
+  if(lstat(file, &st) == 0) return st.st_uid;
+
+  perror("Could not get inode of the file");
+  exit(1);
+}
+
+int getGroupID(char* file)
+{
+  if(lstat(file, &st) == 0) return st.st_gid;
+
+  perror("Could not get inode of the file");
+  exit(1);
+}
+
+int get_iNode(char *file)
+{
+  if(lstat(file, &st) == 0) return st.st_ino;
+
+  perror("Could not get inode of the file");
+  exit(1);
+}
+
+int get_aTime(char *file)
+{
+  if(lstat(file, &st) == 0) return st.st_atime;
+
+  perror("Could not get access time of the file");
+  exit(1);
+}
+
+int get_cTime(char *file)
+{
+  if(lstat(file, &st) == 0) return st.st_ctime;
+
+  perror("Could not get status change time of the file");
+  exit(1);
+}
+
+int get_mTime(char *file)
+{
+  if(lstat(file, &st) == 0) return st.st_mtime;
+
+  perror("Could not get modification of the file");
+  exit(1);
+}
+
+char getExtension(char* COLOR)
+{
+  if(COLOR == COLOR_GREEN) return '*';    	  
+  else if(COLOR == COLOR_LIGHT_BLUE) return '/';
+  else if(COLOR == COLOR_CYAN) return '@';
+
+}
+
+char* getGroupName(char* file)
+{
+  grp = getgrgid(getGroupID(file));
+
+  return grp->gr_name;
+
+  perror("Could not get inode of the file");
+  exit(1);
+}
+
+char* getUserName(char* file)
+{
+  pwd = getpwuid(getUserID(file));
+
+  return pwd->pw_name;
+}
+
+/* END OF GET FUNCTIONS */
+
+
+/* COMPARATORS */
+int cmpName(const void* a, const void* b)
+{
+  fileInfo* x = (fileInfo *) a;
+  fileInfo* y = (fileInfo *) b;
+  
+  int returnValue = strcmp(strlwr(x->name), strlwr(y->name));
+
+  return (!op.sort_reverse) ? returnValue : -returnValue;
+}
+
+
+int cmpTime(const void* a, const void* b)
+{
+  fileInfo* x = (fileInfo *) a;
+  fileInfo* y = (fileInfo *) b;
+  
+  int returnValue;
+
+  if(op.sort_atime) returnValue = x->atime - y->atime;
+  if(op.sort_atime) returnValue = x->ctime - y->ctime;
+  if(op.sort_atime) returnValue = x->mtime - y->mtime;
+
+  return (!op.sort_reverse) ? returnValue : -returnValue;
+}
+
+
+int cmpSize(const void* a, const void* b)
+{
+  fileInfo* x = (fileInfo *) a;
+  fileInfo* y = (fileInfo *) b;
+
+  int returnValue = x->size - y->size;
+  
+  return (!op.sort_reverse) ? returnValue : -returnValue; 
+}
+
 
 char* strlwr(const char* s)
 {
@@ -239,45 +474,11 @@ char* strlwr(const char* s)
   
   return temp;
 }
-
-char* permColor(char *file)
-{
-
-  if(stat(file, &st) == 0)
-  {
-    mode_t perm = st.st_mode;
-
-    if(perm & S_IFDIR) return COLOR_LIGHT_BLUE;
-    if(perm & S_IXUSR) return COLOR_GREEN;
-//    if(perm & S_IFLNK) return COLOR_CYAN;
-	
-    return COLOR_RESET;
-  }
-  else
-  {
-    perror("Could not get permissions of file");
-    return strerror(errno);
-  }   
-}
-
-int inode(char *file)
-{
-  if(stat(file, &st) == 0) return st.st_ino;
-
-  perror("Could not get inode of the file"); 
-}
+/* END OF COMPARATORS */
 
 int isHiddenFile(char* file)
 {
   return (file[0] == '.') ?  1 : 0;
 }
 
-void printExtension(char* COLOR)
-{
-  if(COLOR == COLOR_GREEN) printf("*  ");    	  
-  else if(COLOR == COLOR_LIGHT_BLUE) printf("/  ");
-  else if(COLOR == COLOR_CYAN) printf("@  ");
-}
-
 /* END OF FUNCTIONS */
-
